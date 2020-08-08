7e2444891c838d79797b613f59d7e84715bb7c64
Denis Lykov <cailean393@gmail.com>
2020-08-08 22:46:07 +0300

    add build modes

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,65 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: './src/development/index.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+const mod = process.env.NODE_ENV || 'development';
+
+const options = {
+  umd: {
+    mode: 'production',
+    devtool: false,
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'lib'),
+      filename: 'editor.umd.js',
+      library: 'editor',
+      libraryTarget:'umd',
+      umdNamedDefine: true
+    },
+    externals: ['react', 'react-dom'],
   },
+  browser: {
+    mode: 'production',
+    devtool: false,
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'lib'),
+      filename: 'editor.min.js',
+      library: '',
+      libraryTarget:'window',
+    },
+  },
+  development: {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/development/index.ts',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js',
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src/development/index.html'),
+        inject: true,
+        hash: true,
+      })
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 5000,
+      noInfo: true,
+      open: true,
+      overlay: true,
+      progress: true,
+    },
+  }
+}
+
+module.exports = {
   resolve: {
     extensions: [ '.tsx', '.ts', '.js', '.json' ]
   },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 5000,
-    noInfo: true,
-    open: true,
-    overlay: true,
-    progress: true,
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/development/index.html'),
-      inject: true,
-      hash: true,
-    })
-  ],
   module: {
     rules: [
       {
@@ -66,3 +97,7 @@ module.exports = {
     ]
   }
 };
+
+for (const key of Object.keys(options[mod])) {
+  module.exports[key] = options[mod][key];
+}
